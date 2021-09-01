f1d93c2888e981d0b4c1b211f5e4b774744ae035
markandrewjamito4 <64141186+markandrewjamito4@users.noreply.github.com>
2021-09-01 17:10:34 +0800

    Update mcintro.sh

--- mcintro.sh
+++ mcintro.sh
@@ -1,1 +1,3 @@
-sleep 12 && mpv -fs -hwdec https://www.youtube.com/watch?v=n9P8IOfBksg
+#!/bin/sh
+sleep 12 && mpv -fs -hwdec https://www.youtube.com/watch?v=n9P8IOfBksg # Sleep 12 means 12 seconds before the script triggers so you need to modify this to be able to use the script correctly.
+
